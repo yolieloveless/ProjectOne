cbeffcd643a74b29f570e20f49e8cd3784311ae2
yolieloveless <yolieloveless@gmail.com>
2019-05-10 10:25:25 -0500

    fixed scoreboard

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
 /*----- constants -----*/
 
-/*----- app's state (variables) -----*/
-
-
-/*----- cached element references -----*/
-
-
-/*----- event listeners -----*/
 const lookupABC = ['a', 'b', 'c'];
 
 const abc = {
@@ -22,118 +15,76 @@ const abc = {
 
 };
 
-function random (lookupABC) {
+
+
+/*----- app's state (variables) -----*/
+
+
+/*----- cached element references -----*/
+
+
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
+
+function random(lookupABC) {
+    if (gameScore === "Winner!") return;
     var s = "";
     for (var i = 0; i < lookupABC.length; i++) {
         s += "" + `<img src="${abc[lookupABC[spinSlot()]].imgUrl}">`;
-        }
-        document.getElementById("slotOne").innerHTML = s;
-}
-
-
-// function slotTwo (lookupABC) {
-//         var t = "";
-//         for (var i = 0; i < lookupABC.length; i++) {
-//             s += "" + `<img src="${abc[lookupABC[i]].imgUrl}">`;
-//             }
-//             document.getElementById("slotTwo").innerHTML = t;
-
-
-//         }
-
-           
-//         function slotThree(lookupABC) {
-//         var u = "";
-//             for (var i = 0; i < lookupABC.length; i++) {
-//                 s += "" + `<img src="${abc[lookupABC[i]].imgUrl}">`;
-//                 }
-//                 document.getElementById("slotThree").innerHTML = u;
-//     }
+    }
+    document.getElementById("slotOne").innerHTML = s;
+};
 
 function spinSlot() {
+    if (gameScore === "Winner!") return;
     return Math.floor(Math.random() * lookupABC.length)
-    // lookupABC.sort(function() {
-    //     return 0.5 - Math.random();
-    // });
-}
-
-
-
+};
 
 let scores, winner;
 
-const timerEl = document.querySelector('#timer h2');
-const scoreEl = document.querySelector('#score h2');
+const scoreEl = document.querySelector('h2');
 const slotEl = document.querySelector('#slotMachine div');
 
+const spnBtn = document.querySelector("button").addEventListener('click', handleClick);
 
-document.querySelector('#startBtn').addEventListener('click', playSlot);
-// document.querySelector('#spinBtn').addEventListener('click', spin);
-
-var p = scores
-
-initialize();
-
-function initialize() {
-  scores = {
-    p: 0,
-  };
-
-  winner = null;
-  render();
+function handleClick(evt){
+    if (gameScore === "Winner!") return;
+    gameScoreboard()
+    win()
+    render();
 }
 
-// function getRandomIdx(slotArray) {
-//    return Math.floor((Math.random() * 10) + 1);
-// }
 
-// function spin(gameScore) {
-//     scores.p = abc[getRandomIdx()];
-//     scores.p = abc[getRandomIdx()];
-//     scores.p = abc[getRandomIdx()];
-//     Winner = win();
-//     // scores[winner]++;
-//     render();
-//   }
+var gameScore = 0;
 
-function gameScore() {
-    if ('imgs/cupcake.png' === 'imgs/cupcake.png' | 'imgs/lemon.png' === 'imgs/lemon.png' | 'imgs/pineapple.png' === 'imgs/pineapple.png') {
-        scores = (p + 50) 
-    } else if ('imgs/cupcake.png' === 'imgs/cupcake.png' &&  'imgs/cupcake.png')
-        scores = (p + 100)
-            else if (time === 0 && score < 500)
-                return "LOSER!";
+
+function gameScoreboard() {
+    if (('imgs/cupcake.png' === 'imgs/cupcake.png') || ('imgs/lemon.png' === 'imgs/lemon.png') || ('imgs/pineapple.png' === 'imgs/pineapple.png')) {
+        return gameScore = (gameScore + 50);
+    } else if ('imgs/cupcake.png' === 'imgs/cupcake.png' && 'imgs/cupcake.png') {
+        gameScore = gameScore + 100;
+    }
+    else if (time === 0 && score < 500) {
+        return "LOSER!";
+    }
+    return gameScore;
+
 }
 
 function win() {
-    if (gameScore === 500)
-        return "Winner!";
-}
+    if (gameScore >= 500) { gameScore = "Winner!"};
+    }
 
-function playSlot() {
-    startTmr(startBtn);
-}
 
-function startTmr(cb) {
-    let time = 100;
-    timerEl.textContent = time;
-    let timerId = setInterval(function() {
-        time--;
-        if (time) {
-            timerEl.textContent = time;
-        }else {
-            clearInterval(timerId);
-            timerEl.textContent = '';
-            cb();
-        }
 
-    }, 1000);
-}
 
 function render() {
     // render scores
-    scoreEl.textContent = scores.p;
+    // scoreEl.textContent = gameScore;
+    scoreEl.textContent = gameScore
 
-  
-  }
-  
+
+}
+
